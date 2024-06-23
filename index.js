6d6c9fa25157e5922f9ad74acd49fa4e22cf28a0
Irfanulamin <irfanulamin10@gmail.com>
2024-06-23 20:39:29 +0600

    authentication system has been added

--- index.js
+++ index.js
@@ -25,8 +25,65 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const collection = client.db("ZSTORE").collection("auth");
     // Connect the client to the server
     // await client.connect();
+    app.post("/api/v1/register", async (req, res) => {
+      const { name, email, password } = req.body;
+
+      // Check if email already exists
+      const existingUser = await collection.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({
+          success: false,
+          message: "User already exists",
+        });
+      }
+
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Insert user into the database
+      await collection.insertOne({
+        name,
+        email,
+        password: hashedPassword,
+        type: "user",
+      });
+
+      res.status(201).json({
+        success: true,
+        message: "User registered successfully",
+      });
+    });
+
+    // User Login
+    app.post("/api/v1/login", async (req, res) => {
+      const { email, password } = req.body;
+
+      // Find user by email
+      const user = await collection.findOne({ email });
+      if (!user) {
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
+
+      // Compare hashed password
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
+
+      // Generate JWT token
+      const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, {
+        expiresIn: process.env.EXPIRES_IN,
+      });
+
+      res.json({
+        success: true,
+        message: "Login successful",
+        token,
+      });
+    });
 
     const menCollection = client.db("ZSTORE").collection("mencollection");
 
